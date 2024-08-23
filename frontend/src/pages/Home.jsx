202ed74e0eca7bebce4488e2a9505e01232425cb
izzibizz <contact@izabellind.com>
2024-08-23 15:17:15 +0200

    trying to fix problem with mobile view of video background in deployed version

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,8 @@ export const Home = () => {
         autoPlay
         loop
         muted
-        className="fixed min-w-full min-h-full max-w-none top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "
+        playsInline
+        className="fixed top-1/2 left-1/2 w-screen h-screen object-cover transform -translate-x-1/2 -translate-y-1/2"
       >
         <source src="/Blomdanser.mp4" type="video/mp4" />
       </video>
