7fdcd003c7a932bbe6d2b5c6b14662ae50999bd9
izzibizz <contact@izabellind.com>
2024-08-30 10:37:39 +0200

    debugging testing things

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,27 @@
 import cors from "cors";
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { Project } from "../models/projectSchema";
+import projectData from "../data/projects.json";
+
+dotenv.config();
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/amakyei";
+mongoose.connect(mongoUrl);
+mongoose.Promise = Promise;
+
+if (process.env.RESET_DB === "true") {
+    const seedDatabase = async () => {
+      await Project.deleteMany();
+      console.log("seeding data")
+  
+      projectData.forEach((projectData) => {
+        new Project(projectData).save();
+      });
+    };
+    seedDatabase();
+  }
 
 //Import from seeding so that is runs
 import documentationRoutes from "./routes/documentation";
